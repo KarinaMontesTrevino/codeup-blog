ed38410be2d3638e14b68fcc6cf2b55c3646a85f
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-03-24 14:12:55 -0500

    roll-dice.php

--- app/views/roll-dice.php
+++ app/views/roll-dice.php
@@ -5,11 +5,11 @@
 </head>
 <body>
 	<h1> Your guess is: <?php echo $guess; ?></h1>
-	<h1> Random number is: <?php echo $random; ?>!</h1>
+	<h1> Random number is: <?php echo $random; ?></h1>
     <? if ($guess == $random) : ?>
-    	<h2><?= "There is a match!"; ?></h2>
+    	<h2>There is a match!</h2>
     <? else :?>
-    	<h2><?= "There is not a match!"; ?></h2>
+    	<h2>There is not a match!"</h2>
     <? endif;?>
 </body>
 </html>
