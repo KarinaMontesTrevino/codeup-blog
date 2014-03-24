4fde6ea88ead0981afa10cf557d4622444a38347
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-03-24 13:53:16 -0500

    Modify the route and view so that you can display the guess in addition to the roll and also tell if the guess matches the roll.

--- app/views/roll-dice.php
+++ app/views/roll-dice.php
@@ -4,7 +4,12 @@
     <title> Roll Dice View</title>
 </head>
 <body>
-	<h1> Your guess is: <?php echo $guess; ?><h1>
-    <h1> Random Number is: <?php echo $random; ?>!</h1>
+	<h1> Your guess is: <?php echo $guess; ?></h1>
+	<h2> Random number is: <?php echo $random; ?>!</h1>
+    <? if ($guess == $random) : ?>
+    	<h2><?= "There is a match!"; ?></h2>
+    <? else :?>
+    	<h2><?= "There is not a match!"; ?></h2>
+    <? endif;?>
 </body>
 </html>
