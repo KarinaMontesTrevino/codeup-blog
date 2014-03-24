1cf0d098410c795ebcd2d94b7c5e93c0a4758c01
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-03-24 14:34:49 -0500

    deleting the if from the view

--- app/views/roll-dice.php
+++ app/views/roll-dice.php
@@ -1,15 +1,15 @@
 <!doctype html>
 <html lang="en">
 <head>
+	<meta charset="utf-8">
+	<meta http-equiv="X=UA-Compatible" content="IE-Edge">
     <title> Roll Dice View</title>
+    <meta name = "description" content = "">
+    <meta name = "viewport" content = "width=device-width, initial-scale=1"
 </head>
 <body>
 	<h1> Your guess is: <?php echo $guess; ?></h1>
 	<h1> Random number is: <?php echo $random; ?></h1>
-    <? if ($guess == $random) : ?>
-    	<h2>There is a match!</h2>
-    <? else :?>
-    	<h2>There is not a match!"</h2>
-    <? endif;?>
+    <h2> <?php echo $msg; ?></h2>
 </body>
 </html>
