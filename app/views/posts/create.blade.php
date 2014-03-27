1dad73ab939e8654d70f5d4723fec4edc57d06d9
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-03-27 15:54:27 -0500

    updating create view to use larval form builder

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -2,25 +2,29 @@
 
 @section('content')
 
-<h1>Create a New Post</h1>
-<form class="form-horizontal" role="form" action="{{{ action('PostsController@store') }}}" method = "post">
-	  <div class="form-group">
-		    <label for="title" class="col-sm-2 control-label">Title</label>
-		    <div class="col-sm-10">
-		      <input type="text" class="form-control" id="title" name="title" placeholder="Title" value = "{{{ Input::old('title')}}}">
-		    </div>
-	   </div>
-		  <div class="form-group">
-		    <label for="body" class="col-sm-2 control-label">Body</label>
-		    <div class="col-sm-10">
-		      <textarea id ="body" name="body" class="form-control" rows="5">{{{ Input::old('body')}}}</textarea>
-		    </div>
-		  </div>
-		  <div class="form-group">
-		    <div class="col-sm-offset-2 col-sm-10">
-		      <button type="submit" class="btn btn-default">Create Post</button>
-		    </div>
-	      </div>
-</form>
-
+<h1 class = "blog-title">Create a New Post</h1>
+   <div class = "blog-post">
+    {{ Form::open(array('action' => 'PostsController@store', 'class' => 'form-horizontal')) }}
+	{{ Form::token() }}
+		  <div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}">
+			  {{ Form::label('title', 'Title', array('class'=> 'col-sm-2 control-label'))}}
+			    <div class="col-sm-10">
+                  {{ Form::text('title',  null, array('class' => 'form-control', 'placeholder' => 'Title')) }}
+                  {{  $errors->first('title', '<span class="help-block">:message</span>') }}
+			    </div>
+		   </div>
+			  <div class="form-group {{ $errors->has('body') ? 'has-error' : '' }}">
+			   {{ Form::label('body', 'Body', array('class'=> 'col-sm-2 control-label')) }}
+			    <div class="col-sm-10">
+				  {{ Form::textarea('body', null, array('class' => 'form-control', 'row' => '5')) }}
+			      {{  $errors->first('body', '<span class="help-block">:message</span>') }}
+			    </div>
+			  </div>
+			  <div class="form-group">
+				  <div class="col-sm-offset-2 col-sm-10">
+					  <button type ="submit" class ="btn btn-default">Create Post</button>
+				  </div>
+		      </div>
+	{{ Form::close() }}
+    <div>
 @stop
