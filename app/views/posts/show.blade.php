47f9d5d0b2ab07b9ec82707c2fb5a4af15696dcc
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-03-28 11:29:04 -0500

    changing time zone and format

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -1,13 +1,30 @@
 @extends('layouts.master')
-
 @section('content')
 
           
             	<h3 class="blog-post-title">{{{$post->title}}}</h3>
-                <p class="blog-post-meta">{{{$post->created_at}}}</p>
             	<p>{{{$post->body}}}</p>
+                <p class="pull-right">{{{$post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i A')}}}</p>
 
-            	<hr>
-            	<p><h4><a href = "{{{action ('PostsController@index') }}}">Return to posts listings</a> &middot; <a href = "{{{action ('PostsController@edit', $post->id) }}}">Edit Post</a></h4></p> 
+            	<br><hr>
+            	<p><h4><a href = "{{{ action ('PostsController@index') }}}">Return to posts listings</a> | 
+            		   <a href = "{{{ action ('PostsController@edit', $post->id) }}}">Edit Post</a> &middot;
+            		   <a href = "#" id = "btnDeletePost"> Delete Post</a></h4></p> 
+                
+                <!-- form for deleting a post -->
+                {{ Form::open(array('action'=> array('PostsController@destroy', $post->id), 'method' => 'delete', 'id' => 'formDeletePost'))}}
+                {{ Form::close() }}
+@stop
 
+@section('bottom-script')
+	<script>
+	$('#btnDeletePost').on('click', function(e){
+	   e.preventDefault();
+
+	   if (confirm('Are you sure you want to delete this post?')){
+	   	  $('#formDeletePost').submit();
+	   }
+
+	});
+	</script>
 @stop
