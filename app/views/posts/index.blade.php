7b7e784b6cb63270bdf2ce822f9c3be7594d59f0
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-03-28 13:05:44 -0500

    adding edit next to every post

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -1,23 +1,37 @@
 @extends('layouts.master')
+<style>
+  .inline{
+  	display: inline-block;
+  }
 
+  .btn.btn-primary-sm{
+  	padding: 0px;
+  }
+  
+</style>
 @section('content')
 
 	<h1 class="blog-title">Technologic Blog</h1><span class="lead">Technology at your fingertips</span> 
-       <p>
-            <h4><a href="{{{action ('PostsController@create') }}}"> Create New Post</a></h4>
-       </p>
-       <hr>
+			 <p>
+						<h4><a href="{{{action ('PostsController@create') }}}"> Create New Post</a></h4>
+			 </p>
+			 <hr>
 
- 	@foreach ($posts as $post)
+	@foreach ($posts as $post)
 		<div class="blog-post">
-            	<h3 class="blog-post-title"><a href = "{{{ action('PostsController@show', $post->id) }}}">{{{ $post->title }}}</a></h3>
-            	<p>{{{ Str::words($post->body, 100) }}}</p>
-              <p class= "pull-right">{{$post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i A')}}</p>
 
-            	<br><hr>
-    	</div>
- 	@endforeach
+					<div class ="inline">
+						<h3 class="blog-post-title"><a href = "{{{ action('PostsController@show', $post->id) }}}">{{{ $post->title }}}</a>
+						<a type= "button" class = "btn btn-primary-sm" href = "{{{ action('PostsController@edit', $post->id) }}}">&middot; Edit</a></h3>
+					</div>
 
-  {{ $posts->links() }}
-    
+					<p>{{{ Str::words($post->body, 100) }}}</p>
+					<p class= "pull-right">{{$post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i A')}}</p>
+
+					<br><hr>
+			</div>
+	@endforeach
+
+	{{ $posts->links() }}
+		
 @stop
