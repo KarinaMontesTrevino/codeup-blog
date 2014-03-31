d0dd86feabced63c4dc81bdac7df1a968567955c
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-03-31 15:44:29 -0500

    changing my form back to initial phase

--- app/views/contact.blade.php
+++ app/views/contact.blade.php
@@ -54,54 +54,63 @@
  </script>
 @section('content')	
 <body onload="initialize()">
-<div id="map_canvas"></div>
-   <div id ="cont-form">
-   	    <p><h2 class="text-center"> Say Hello!</h2></p>
-		<form class="form-horizontal" role="form">
-			  <div class="form-group">
-				    <label for="name" class="col-sm-3 control-label">Name</label>
-				    <div class="col-lg-8">
-				      	<input type="text" class="form-control" id="name" placeholder="Name">
-				    </div>
-			   </div>
-			   <div class="form-group">
-				    <label for="subject" class="col-sm-3 control-label">Subject</label>
-				    <div class="col-lg-8">
-				      	<input type="text" class="form-control" id="subject" placeholder="Subject">
-				    </div>
-			   </div>
-			     <div class="form-group">
-				    <label for="email" class="col-sm-3 control-label">Email</label>
-				    <div class="col-lg-8">
-				      	<input type="text" class="form-control" id="email" placeholder="Email">
-				    </div>
-			   </div>
-			   <div class="form-group">
-			   	    <label for="message" class="col-sm-3 control-label">Message</label>
-			   	    <div class="col-lg-8">
-			          	<textarea class="form-control" rows="5"></textarea>
-			       </div>
-			   </div>
-			  <div class="form-group">
-			    	<div class="col-sm-offset-3 col-sm-8">
-			      		<button type="submit" class="btn btn-primary">Send</button>
-			    	</div>
-			  </div>
-		</form>
-    </div>
-    <div id = "address" class="col-lg-12 col-sm-offset-0 blog-sidebar">
-				<address>
-				  <strong>Codeup.</strong><br>
-				  112 E. Pecan St.<br>
-				  San Antonio, TX 78205<br>
-				  <abbr title="Phone">P:</abbr> (123) 456-7890
-				</address>
+	<div style="margin-top:0px">
+ <div id="map_canvas"></div>
+			<div class="container">
+				<div class="panel panel-default" style="margin-left:580px; width:500px">
+					<div class="panel-heading">
+						<h2 class="panel-title">Contact Form</h2>
+					</div>
+					<div class="panel-body">
+						<form name="contactform" method="post" action= class="form-horizontal" role="form">
+							<div class="form-group">
+								<label for="inputName" class="col-lg-2 control-label">Name</label>
+								<div class="col-lg-10">
+									<input type="text" class="form-control" id="inputName" name="inputName" placeholder="Your Name">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="inputEmail1" class="col-lg-2 control-label">Email</label>
+								<div class="col-lg-10">
+									<input type="text" class="form-control" id="inputEmail" name="inputEmail" placeholder="Your Email">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="inputSubject" class="col-lg-2 control-label">Subject</label>
+								<div class="col-lg-10">
+									<input type="text" class="form-control" id="inputSubject" name="inputSubject" placeholder="Subject Message">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="inputPassword1" class="col-lg-2 control-label">Message</label>
+								<div class="col-lg-10">
+									<textarea class="form-control" rows="4" id="inputMessage" name="inputMessage" placeholder="Your message..."></textarea>
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="col-lg-offset-2 col-lg-10">
+									<button type="submit" class="btn btn-primary">
+										Send Message
+									</button>
+								</div>
+							</div>
+						</form>
 
-				<address>
-				  <strong>Karina Montes</strong><br>
-				  <a href="mailto:#">karina_montes-trevino@hotmail.com</a>
-				</address>
-    </div>
-
+					</div>
+				</div>
+			</div>
+		</div>
 </body>
+
+<address>
+  <strong>Codeup.</strong><br>
+  112 E. Pecan St.<br>
+  San Antonio, TX 78205<br>
+  <abbr title="Phone">P:</abbr> (123) 456-7890
+</address>
+
+<address>
+  <strong>Karina Montes</strong><br>
+  <a href="mailto:#">first.last@example.com</a>
+</address>
 @stop
