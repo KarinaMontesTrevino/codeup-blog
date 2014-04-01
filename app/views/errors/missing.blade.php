0f42e1c0cbe595f74b8b4e8595a0a972640ad25f
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-04-01 18:51:20 -0500

    fixing errors

--- app/views/errors/missing.blade.php
+++ app/views/errors/missing.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.master')
 	<style>
-		#error{
+		div #error{
          	margin-left: 40px;
          	height: 500px;
          	width: 1060px;
 		}
 	</style>
 @section('content')
+  <div>
     <img id="error" src = "/img/gravity.jpg">
+  </div>
 @stop
